7265da71bb05bdae6bae4c3192e1a25d5f0e3643
Imdaadh <67889560+Imdaadh@users.noreply.github.com>
2022-05-18 19:16:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Simple Next.js demo! See Which is Done Using Docker,Kubernetes & Google Cloud -IT19148250 <Link href="/cars">Cars</Link>
+          Simple Next.js demo! See Which is Done Using Docker,Kubernetes & gCloud - IT19148250 <Link href="/cars">Cars</Link>
         </h1>
         </main>
     </div>
